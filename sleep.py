a06580058331edfd33ef15020dc44dec2c284f9e
Frank A <frank.alvarez.a14@gmail.com>
2022-02-24 15:57:55 -0500

    Update to file name and arguments

--- sleep.py
+++ sleep.py
@@ -1,20 +1,21 @@
 from db import db
 from google.cloud import firestore
 
-
+# Firestore object
 sleep_record_ref = db.collection(u'sleep')
 
-
-def createSleepRecord(userid, date=firestore.SERVER_TIMESTAMP, start_time=None, end_time=None, sleep_rating=None, dream_rating=None, sleep_event=None):
+# Create Sleep Record
+def createSleepRecord(userid, start_time=None, end_time=None, sleep_rating=None, dream_rating=None, sleep_event=None):
     sleep_dict = dict()
     sleep_dict['userid'] = userid
-    sleep_dict['date'] = date
+    sleep_dict['date'] = firestore.SERVER_TIMESTAMP
     sleep_dict['start_time'] = start_time
     sleep_dict['end_time'] = end_time
     sleep_dict['sleep_rating'] = sleep_rating
     sleep_dict['dream_rating'] = dream_rating
     sleep_dict['sleep_event'] = sleep_event
 
+    # Add Sleep Record to database
     sleep_record = sleep_record_ref.add(sleep_dict)
     
     return
