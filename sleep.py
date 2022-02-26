0eaa1d477dbac47eea5592b90ac8d7ad05ae09b7
Frank A <frank.alvarez.a14@gmail.com>
2022-02-25 22:22:13 -0500

    Update sleep.py
    Added update and delete functionality to sleep records.

--- sleep.py
+++ sleep.py
@@ -4,21 +4,22 @@ from google.cloud import firestore
 # Firestore object
 sleep_record_ref = db.collection(u'sleep')
 
-# Create Sleep Record
-def createSleepRecord(userid, start_time=None, end_time=None, sleep_rating=None):
+
+def createSleepRecord(userid, start_time=None, end_time=None, sleep_rating=None, last_modified=None):
     sleep_dict = dict()
     sleep_dict['userid'] = userid
     sleep_dict['date'] = firestore.SERVER_TIMESTAMP
     sleep_dict['start_time'] = start_time
     sleep_dict['end_time'] = end_time
     sleep_dict['sleep_rating'] = sleep_rating
+    sleep_dict['last_modified'] = last_modified
 
-    # Add Sleep Record to database
     sleep_record = sleep_record_ref.add(sleep_dict)
-    
-    return "Done"
 
-# Get all Sleep Records for a given user
+
+    return print(sleep_record)
+
+
 def getUserSleepRecords(user_id):
 
     docs_ref = db.collection(u"sleep").where(u'userid', u'==', user_id).order_by(u"date", direction=firestore.Query.DESCENDING)
@@ -58,4 +59,26 @@ def getLastThirty(user_id):
         list.append(doc.to_dict())
 
     return list
-#
+    
+
+
+def updateSleepRecord(document_id, start_time=None, end_time=None, sleep_rating=None):
+
+    doc = sleep_record_ref.document('document_id')
+    
+    sleep_dict = dict()
+    sleep_dict['start_time'] = start_time
+    sleep_dict['end_time'] = end_time
+    sleep_dict['sleep_rating'] = sleep_rating
+    sleep_dict['last_updated'] = firestore.SERVER_TIMESTAMP
+    
+    doc.update(sleep_dict)
+
+    return 
+
+
+def deleteSleepRecord(document_id):
+    
+    doc = sleep_record_ref.document(document_id).delete()
+
+    return "Done"
